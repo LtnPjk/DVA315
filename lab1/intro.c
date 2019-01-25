1fe9cc320ef5fd6c0c393245d54a00bad9f9adcf
felixsjo <felix.sjqv@gmail.com>
2019-01-25 15:31:37 +0100

    intro2 = assignment_2

--- lab1/intro.c
+++ lab1/intro.c
@@ -1,7 +1,19 @@
 #include "wrapper.h"
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void * hellomoon(void * u){
+    while(1){
+    printf("Hello moon!\n");
+    usleep(200000);
+    }
+}
 
 int main(){
+    pthread_t tid;
+    pthread_create(&tid, NULL, hellomoon, NULL);
     for(int i = 0; i < 10; i++){
         printf("Hello world!\n");
         sleep(1);
