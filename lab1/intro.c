24cc3ee046533ad5ec04e428a18705b6c5fd143b
LtnPjk <olle.olofsson97@gmail.com>
2019-01-30 10:25:32 +0100

    Oof4

--- lab1/intro.c
+++ lab1/intro.c
@@ -4,22 +4,36 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+pthread_mutex_t lock;
+pthread_t TID;
+
 void * hellomoon(void* u){
-    for(int k = 0; k < 10; k++){
-    printf("%s", (char*)u);
-    usleep(200000);
+    while(1){
+        pthread_mutex_lock(&lock);
+        for(int k = 0; k < 10; k++){
+            printf("Hello moon!\n");
+            usleep(200000);
+        }
+        pthread_mutex_unlock(&lock);
+        usleep(1000);
     }
 }
 
 int main(){
-    pthread_t tid;
-    char * word = "Hello Moon!\n";
+    if(pthread_mutex_init(&lock, NULL) != 0){
+        printf("Mutex could not be created!\n");
+        return 0;
+    }
+    pthread_create(&TID, NULL, &hellomoon, NULL);
     while(1){
+        if(pthread_mutex_lock(&lock)!=0){
+            printf("QWEQWEQWE\n");
+        }
         for(int i = 0; i < 10; i++){
             printf("Hello World!\n");
             sleep(1);
         }
-        pthread_create(&tid, NULL, &hellomoon, word);
-        pthread_join(tid, NULL);
+        pthread_mutex_unlock(&lock);
+        usleep(1000);
     }
 }
