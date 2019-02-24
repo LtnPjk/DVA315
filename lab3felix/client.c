ddfc58054e0822478e9ab82f8ae16825166992cb
felixsjo <felix.sjqv@gmail.com>
2019-02-24 15:21:52 +0100

    lab3felix

---
+++ lab3felix/client.c
@@ -0,0 +1,32 @@
+#include "COMMON/wrapper.h"
+
+#define QUEUE_NAME "/mq1"
+
+int main()
+{
+    //Implement you inter process communication here, happy coding
+    planet_type planet;
+    mqd_t mq;
+
+    while(1){
+        printf("---  Create Planet  ---\n");
+        printf("Name:\tx-pos\ty-pos\tx-vel\ty-vel\tlife\n");
+        scanf("%s",  planet.name);
+        scanf("%lf", &planet.sx);
+        scanf("%lf", &planet.sy);
+        scanf("%lf", &planet.vx);
+        scanf("%lf", &planet.vy);
+        scanf("%lf", &planet.mass);
+        scanf("%d", &planet.life);
+        printf("Spawning planet...\n");
+
+        if(MQconnect(&mq, QUEUE_NAME) != 1){
+            printf("CLIENT: Could not connect!\n");
+        }
+        else{
+            printf("CLIENT: Connected!\n");
+            MQwrite(&mq, (void *)&planet);
+            printf("CLIENT: Request sent!\n");
+        }
+    }
+}
